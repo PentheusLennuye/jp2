3fc7502d2eb6d502dd82975e83fd61c4b0bca1ba
PentheusLennuye <gmc@cummings-online.ca>
2021-05-13 21:56:28 -0400

    PoC now has nice playfield, but white background missing

--- tutorial/poc.py
+++ tutorial/poc.py
@@ -12,19 +12,19 @@ default_font.setPointSize(18)
 gloss_font = QFont()
 gloss_font.setPointSize(10)
 
-h1fontsize = 24
-pfontsize = 16
+h1fontsize = 18
+pfontsize = 14
 
 JP = 2
 EN = 0
 
 title = 'Chapter Four, Exercise 2b'
 instructions = 'Translate into Japanese.'
-question = 'The library is behind the university.'
-answer = [
-            ['図書館', 'としょかん'], ['は', ''], ['大学', 'だいがく'],
-            ['の', ''], ['後ろ', 'うし'], ['です', '']
-         ]
+questions = ['The library is behind the university.', ]
+answers = [
+            [['図書館', 'としょかん'], ['は', ''], ['大学', 'だいがく'],
+            ['の', ''], ['後ろ', 'うし'], ['です', '']],
+]
 
 
 class Title(QLabel):
@@ -72,53 +72,58 @@ class PlayfieldSection(QWidget):
 
         self.setStyleSheet('background-color: white;')
         layout = QHBoxLayout()
-        layout.addStretch(1)
         layout.setSpacing(0)
-        layout.setAlignment(Qt.AlignLeft)
+        layout.setAlignment(Qt.AlignCenter)
 
         if isinstance(text, str):
             widget = QLabel(text, self)
-            self.setFont(default_font)
+            widget.setFont(default_font)
             layout.addWidget(widget)
 
         if isinstance(text, list):
             for t in text:
                 if isinstance(t, str):
                     widget = QLabel(t, self)
-                    layout.addWidget(widget)
+                elif isinstance(t, list):
+                    widget = GlossBox(t[0], t[1], self)
+                widget.setFont(default_font)
+                layout.addWidget(widget)
 
         self.setLayout(layout)
 
 
-class Card(QDialog):
-    def __init__(self, title, instructions, parent=None):
+class Playfield(QWidget):
+    def __init__(self, question, answer, parent):
         super().__init__(parent)
+        self.setStyleSheet('background-color: white;')
+        qwidget = PlayfieldSection(question, self)
+        awidget = PlayfieldSection(answer, self)
+
+        playfield_layout = QVBoxLayout()
+        playfield_layout.setAlignment(Qt.AlignCenter)
+        playfield_layout.addWidget(qwidget)
+        playfield_layout.addWidget(awidget)
+        self.setLayout(playfield_layout)
+
+
+class Card(QDialog):
+    def __init__(self, title, instructions,
+                 questions, answers, parent=None):
+        super().__init__(parent)
+        self.index = 0
         self.setWindowTitle('日本語の練習')
 
-        self.title = Title(title, self)
-        self.instructions = Instructions(instructions, self)
-
-        self.playfield = QWidget()
-        self.playfield = QVBoxLayout()
-        self.playfield.setAlignment(Qt.AlignCenter)
-
-
-        self.question = PlayfieldSection(
-            'It is the Japanese language.', self
+        title = Title(title, self)
+        instructions = Instructions(instructions, self)
+        self.playfield = Playfield(
+            questions[self.index], answers[self.index], self
         )
-        self.answer = PlayfieldSection(
-            ['日本語', 'です'],
-            self
-        )
-
-        # self.answer = GlossBox('日本語', 'にほんご', self)
 
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignCenter)
-        layout.addWidget(self.title)
-        layout.addWidget(self.instructions)
-        layout.addWidget(self.question)
-        layout.addWidget(self.answer)
+        layout.addWidget(title)
+        layout.addWidget(instructions)
+        layout.addWidget(self.playfield)
         # layout.addWidget(self.button)
         # layout.addWidget(self.quit)
         self.setLayout(layout)
@@ -126,6 +131,6 @@ class Card(QDialog):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    card = Card(title, instructions)
+    card = Card(title, instructions, questions, answers)
     card.show()
     sys.exit(app.exec())
