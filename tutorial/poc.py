d624fe1751ad2ca5f95348e647a01376536b0e42
PentheusLennuye <gmc@cummings-online.ca>
2021-05-19 09:14:42 -0400

    POC complete

--- tutorial/poc.py
+++ tutorial/poc.py
@@ -4,7 +4,7 @@ import sys
 from PySide6.QtWidgets import (QApplication, QDialog, QPushButton, QLabel,
                                QVBoxLayout, QHBoxLayout, QWidget)
 from PySide6.QtGui import QFont
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, Slot, QCoreApplication
 
 
 default_font = QFont()
@@ -17,14 +17,6 @@ pfontsize = 14
 
 JP = 2
 EN = 0
-
-title = 'Chapter Four, Exercise 2b'
-instructions = 'Translate into Japanese.'
-questions = ['The library is behind the university.', ]
-answers = [
-            [['図書館', 'としょかん'], ['は', ''], ['大学', 'だいがく'],
-            ['の', ''], ['後ろ', 'うし'], ['です', '']],
-]
 
 
 class Title(QLabel):
@@ -63,6 +55,7 @@ class GlossBox(QWidget):
         layout = QVBoxLayout()
         layout.addWidget(self.gloss_label)
         layout.addWidget(self.main_label)
+        layout.setContentsMargins(1, 1, 1, 1)
         self.setLayout(layout)
 
 
@@ -70,7 +63,6 @@ class PlayfieldSection(QWidget):
     def __init__(self, text, parent):
         super().__init__(parent)
 
-        self.setStyleSheet('background-color: white;')
         layout = QHBoxLayout()
         layout.setSpacing(0)
         layout.setAlignment(Qt.AlignCenter)
@@ -95,15 +87,46 @@ class PlayfieldSection(QWidget):
 class Playfield(QWidget):
     def __init__(self, question, answer, parent):
         super().__init__(parent)
-        self.setStyleSheet('background-color: white;')
-        qwidget = PlayfieldSection(question, self)
-        awidget = PlayfieldSection(answer, self)
+        self.qwidget = PlayfieldSection(question, self)
+        self.awidget = PlayfieldSection(answer, self)
 
         playfield_layout = QVBoxLayout()
         playfield_layout.setAlignment(Qt.AlignCenter)
-        playfield_layout.addWidget(qwidget)
-        playfield_layout.addWidget(awidget)
+        playfield_layout.addWidget(self.qwidget)
+        playfield_layout.addWidget(self.awidget)
         self.setLayout(playfield_layout)
+
+    def hide_answer(self):
+        self.awidget.hide()
+
+    def show_answer(self):
+        self.awidget.show()
+
+
+class Controls(QWidget):
+    def __init__(self, parent, init_label, mainfunction, quitfunction):
+        super().__init__(parent)
+        self.button = QPushButton(init_label)
+        self.button.clicked.connect(mainfunction)
+        self.button.setAutoDefault(True)
+        quitbutton = QPushButton('終わる')
+        quitbutton.setStyleSheet('background-color: #c00; color: white;')
+        quitbutton.clicked.connect(quitfunction)
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.button)
+        layout.addWidget(quitbutton)
+        self.setLayout(layout)
+
+    def set_button(self, label):
+        self.button.setText(label)
+
+    def hide_button(self):
+        self.button.hide()
+
+
+POSING_QUESTION = 0
+SHOWING_ANSWER = 1
 
 
 class Card(QDialog):
@@ -111,26 +134,76 @@ class Card(QDialog):
                  questions, answers, parent=None):
         super().__init__(parent)
         self.index = 0
+        self.mode = POSING_QUESTION
+        self.button_labels = ['答え', '次の質問']
+        self.last_question = len(questions) - 1
         self.setWindowTitle('日本語の練習')
 
-        title = Title(title, self)
-        instructions = Instructions(instructions, self)
+        self.title = Title(title, self)
+        self.instructions = Instructions(instructions, self)
         self.playfield = Playfield(
             questions[self.index], answers[self.index], self
         )
+        self.playfield.hide_answer()
+        self.controls = Controls(self, self.button_labels[self.mode],
+                                 self.move_forward, self.quit_me)
+        self.layout = QVBoxLayout()
+        self.set_layout()
 
-        layout = QVBoxLayout()
-        layout.setAlignment(Qt.AlignCenter)
-        layout.addWidget(title)
-        layout.addWidget(instructions)
-        layout.addWidget(self.playfield)
-        # layout.addWidget(self.button)
-        # layout.addWidget(self.quit)
-        self.setLayout(layout)
+    def set_layout(self):
+
+        self.layout.takeAt(0)
+        self.layout.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.title)
+        self.layout.addWidget(self.instructions)
+        self.layout.addWidget(self.playfield)
+        self.layout.addWidget(self.controls)
+        self.setLayout(self.layout)
+
+    @Slot()
+    def move_forward(self):
+        if self.mode == POSING_QUESTION:
+            self.mode = SHOWING_ANSWER
+            self.show_answer()
+        else:
+            self.mode = POSING_QUESTION
+            self.pose_next_question()
+
+    def pose_next_question(self):
+        self.index += 1
+        self.playfield.setParent(None)
+        self.playfield = Playfield(
+            questions[self.index], answers[self.index], self
+        )
+        self.playfield.hide_answer()
+        self.controls.set_button(self.button_labels[self.mode])
+        self.set_layout()
+
+    def show_answer(self):
+        print("Showing answer")
+        self.playfield.show_answer()
+        if self.index < self.last_question:
+            self.controls.set_button(self.button_labels[self.mode])
+        else:
+            self.controls.hide_button()
+
+    @Slot()
+    def quit_me(self):
+        QCoreApplication.instance().quit()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    title = 'Chapter Four, Exercise 2b'
+    instructions = 'Translate into Japanese.'
+    questions = ['The library is behind the university.',
+                 'The hospital is in front of the department store.']
+    answers = [
+            [['図書館', 'としょかん'], ['は', ''], ['大学', 'だいがく'],
+             ['の', ''], ['後ろ', 'うし'], ['です', '']],
+            [['病院', 'びょういん'], ['は', ''], ['デパート', ''],
+             ['の', ''], ['前', 'まえ'], ['です', '']]
+    ]
     card = Card(title, instructions, questions, answers)
     card.show()
     sys.exit(app.exec())
